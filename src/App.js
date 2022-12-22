ab7088d62a38db55ec87172f5100bf601123e651
abidin angga <abidinangga89@gmail.com>
2022-12-22 11:05:30 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./assets/css/app.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="text-xl"> halo guys</h1>
+    </div>
+  
+  );
+}
+
+export default App;
